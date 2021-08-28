cdb1df9ca5f1be4a6094a98b1bea0a3d54f3880e
Leandro Gonçalves Vanelli <55560279+LeoVanelli@users.noreply.github.com>
2021-08-28 01:02:38 -0300

    v1.0

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OlaUsuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercício 1 - Faça um programa solicite que o usuário digite seu nome. Exiba Olá, ___! (na lacuna ___ exiba o nome digitado).
+
+            string nome; 
+
+            // Pergunta o nome do usuário para apresentar posteriomente.
+            Console.Write("Digite seu nome: ");
+            nome = Console.ReadLine(); 
+
+            Console.WriteLine("Olá,{0}",nome); // Resolução do exercício
+
+            Console.Write("Pressione qualquer tecla para fechar. . .");
+            Console.ReadKey();
+        }
+    }
+}
